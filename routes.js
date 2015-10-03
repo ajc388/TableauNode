eb5b8144e8145363092604ccfbcb08d057120453
Z3R0_H0 <ajc388@nau.edu>
2015-10-03 12:01:09 -0500

    Setup the connector and simulator to work together on the same page. Note: find better way to route assets!

--- routes.js
+++ routes.js
@@ -1,9 +1,22 @@
 module.exports = function (app) {
 	
 	//GET Routes
-	app.get('/', function(req, res) {
+	app.get('/StockConnector.html', function(req, res) {
+	  res.sendFile(__dirname + "/StockConnector.html");
+	});
+
+	app.get('/simulator.html', function(req, res) {
 	  res.sendFile(__dirname + "/Simulator.html");
 	});
+
+	app.get('/simulator.css', function(req, res) {
+	  res.sendFile(__dirname + "/assets/css/simulator.css");
+	});
+
+	app.get('/simulator.js', function(req, res) {
+	  res.sendFile(__dirname + "/assets/js/simulator.js");
+	});
+
 
 	//POST Routes
 }
